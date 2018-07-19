e994a67a9f11bab43a181265bef7d3801668a976
Kousuke Kikumitsu <kske0415@gmail.com>
2018-07-19 09:50:25 +0000

    repair registration scout_meid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
     #パラメーターnameを渡せるようにする
     #
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
     end 
 
 end
