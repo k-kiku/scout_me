4ab41854f17ca2994e47cf8eda2b88f1069121fd
Kousuke Kikumitsu <kske0415@gmail.com>
2018-07-23 13:49:33 +0000

    return application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,8 @@ class ApplicationController < ActionController::Base
     #パラメーターnameを渡せるようにする
     #
     def configure_permitted_parameters
-      added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs 
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end 
 
 end
- 
