a792c0ff5b0891b4ee83846710d4665333e17dd7
Kousuke Kikumitsu <kske0415@gmail.com>
2018-08-20 10:55:53 +0000

    will do scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users, controllers: { omniauth_callbacks:  'users/omniauth_callbacks'}
    
   root                                    'static_pages#index'
@@ -8,9 +9,12 @@ Rails.application.routes.draw do
   get '/users/sign_out',              to: 'devise/sessions#destroy'
   get '/users/sign_in',               to: 'devise/sessions#new' 
   post'/users/sign_in',               to: 'devise/sessions#create'
+  get '/posts/new',                   to: 'posts#new'
+  post'/posts',                       to: 'posts#create'
+  get '/confirm/:id',                 to: 'posts#confirm', as: :confirm
   #@TODO 退会する時
-  #get '/users/cancel',                to: 'devise/registrations#cancel'
-
+  #get '/users/cancel',                to: 'devise/registrations#cancel' 
+  
  
   
   # For details on the DSL available within this file, see 
