75dac0ca070bd7c88bad0930eb84871c1f968238
Kousuke Kikumitsu <kske0415@gmail.com>
2018-10-27 09:24:04 +0000

    error handling app/views/posts/index.html.erb

--- db/migrate/20180806075307_create_posts.rb
+++ db/migrate/20180806075307_create_posts.rb
@@ -1,9 +1,9 @@
-class CreateTweets < ActiveRecord::Migration[5.0]
+class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.text :content
       t.string :picture
-      t.string :tweet_id
+      t.string :id
       t.timestamps
     end
   end
